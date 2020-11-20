b1bb059ce0c339fc73d45f716b7c8f8bfb035935
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-11-19 22:14:14 -0700

    Remove tests that appear in doctests

--- test/counting_test.exs
+++ test/counting_test.exs
@@ -1,37 +1,4 @@
 defmodule Ballot.CountingTest do
   use ExUnit.Case
-  alias Ballot.RankedVote
-  alias Ballot.Counting
   doctest Ballot.Counting
-
-  describe "instant_runoff/2" do
-    test "selects a winner by first choice" do
-      assert "1" == Counting.instant_runoff([
-        RankedVote.new(["1"]),
-        RankedVote.new(["1"]),
-        RankedVote.new(["2"])
-      ])
-    end
-
-    test "selects a winner by second choice" do
-      assert "3" == Counting.instant_runoff([
-        RankedVote.new(["1", "3"]),
-        RankedVote.new(["2", "3"]),
-        RankedVote.new(["3"]),
-        RankedVote.new(["3"])
-      ])
-    end
-
-    test "selects a winner by second choice with configurable win percentage" do
-      assert "6" == Counting.instant_runoff([
-        RankedVote.new(["1", "6"]),
-        RankedVote.new(["1", "6"]),
-        RankedVote.new(["1", "6"]),
-        RankedVote.new(["2", "6"]),
-        RankedVote.new(["3", "6"]),
-        RankedVote.new(["4", "6"]),
-        RankedVote.new(["5", "6"]),
-      ], win_percentage: 75)
-    end
-  end
 end
