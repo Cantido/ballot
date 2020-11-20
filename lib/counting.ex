51d7593b53a1691e3835931b48a89e13d7038cfd
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-11-19 22:13:54 -0700

    Implement Borda count

--- lib/counting.ex
+++ lib/counting.ex
@@ -105,4 +105,66 @@ defmodule Ballot.Counting do
       do_instant_runoff(ranked_votes, worst_candidates ++ losers, opts)
     end
   end
+
+  @doc """
+  Counts votes based on points granted to higher-ranked candidates.
+
+  Each vote grants points to all candidates based on their rank in the vote.
+  For example, in this vote:
+
+      Ballot.RankedVote.new(["1", "2"])
+
+  Candidate "1" is granted two points, and candidate "2" is granted one point.
+
+  ## Examples
+
+  In this example, candidate "2" wins with five points.
+  Candidate "1" is in second place with four points,
+  and candidate "3" is last with three points.
+
+      iex> votes = [
+      ...>   Ballot.RankedVote.new(["1", "2", "3"]),
+      ...>   Ballot.RankedVote.new(["2", "3", "1"]),
+      ...> ]
+      iex> Ballot.Counting.borda(votes)
+      ["2"]
+
+  You also start the counting at zero, instead of one, so the last place choice gets zero points.
+  In the previous example, the winner is the same, but the points end up different.
+  Candidate "2" still wins, but with three points,
+  candidate "1" gets two points, and candidate "3" gets one point.
+
+      iex> votes = [
+      ...>   Ballot.RankedVote.new(["1", "2", "3"]),
+      ...>   Ballot.RankedVote.new(["2", "3", "1"]),
+      ...> ]
+      iex> Ballot.Counting.borda(votes, starting_at: 0)
+      ["2"]
+  """
+  def borda(ranked_votes, opts \\ []) do
+    starting_at = Keyword.get(opts, :starting_at, 1)
+    if starting_at not in [0, 1] do
+      raise ":starting_at must be 0 or 1"
+    end
+
+    points =
+      ranked_votes
+      |> Enum.flat_map(fn vote ->
+        count = Enum.count(vote.choices)
+
+        vote.choices
+        |> Enum.with_index()
+        |> Enum.map(fn {choice, index} -> {choice, count - index + (starting_at - 1)} end)
+      end)
+      |> Enum.reduce(%{}, fn {choice, points}, acc ->
+        Map.update(acc, choice, points, &(&1 + points))
+      end)
+
+    {_best_candidate, best_points} =
+      Enum.max_by(points, fn {_choice, points} -> points end)
+
+    # detect ties, report all winners
+    Enum.filter(points, fn {_choice, points} -> points == best_points end)
+    |> Enum.map(&elem(&1, 0))
+  end
 end
