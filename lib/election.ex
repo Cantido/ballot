2b0b37c9ffe2b05f0d8f348ccc72cf15ee309813
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-01-02 11:51:30 -0700

    Validate vote type when adding a vote

--- lib/election.ex
+++ lib/election.ex
@@ -17,6 +17,15 @@ defmodule Ballot.Election do
   Add a vote to an election.
   """
   def vote(election, vote) do
+    if Enum.count(election.votes) > 0 do
+      first_vote_type = List.first(election.votes).__struct__
+      new_vote_type = vote.__struct__
+
+      unless first_vote_type == new_vote_type do
+        raise "Cannot mix vote types. Given election had a vote of type #{first_vote_type} but was given a new vote of type #{new_vote_type}."
+      end
+    end
+
     Map.update!(election, :votes, fn votes ->
       [vote | votes]
     end)
--- test/election_test.exs
+++ test/election_test.exs
@@ -1,4 +1,14 @@
 defmodule Ballot.ElectionTest do
   use ExUnit.Case
   doctest Ballot.Election
+
+  test "raises when vote types are mixed" do
+    election =
+      Ballot.Election.new(["A"])
+      |> Ballot.Election.vote(Ballot.PluralityVote.new("A"))
+
+    assert_raise RuntimeError, fn ->
+      Ballot.Election.vote(election, Ballot.ApprovalVote.new(["A"]))
+    end
+  end
 end
