2b39e0733c8c09fd2231b72985387f93023789fe
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-01-03 13:25:38 -0700

    Test Election happy path

--- test/election_test.exs
+++ test/election_test.exs
@@ -2,6 +2,16 @@ defmodule Ballot.ElectionTest do
   use ExUnit.Case
   doctest Ballot.Election
 
+  test "adding a vote" do
+    election =
+      Ballot.Election.new(["A"])
+      |> Ballot.Election.vote(Ballot.PluralityVote.new("A"))
+      |> Ballot.Election.vote(Ballot.PluralityVote.new("A"))
+      |> Ballot.Election.vote(Ballot.PluralityVote.new("B"))
+
+    assert ["A"] = Ballot.Counting.plurality(election.votes)
+  end
+
   test "raises when vote types are mixed" do
     election =
       Ballot.Election.new(["A"])
