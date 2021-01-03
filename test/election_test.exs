8f72f8b7145caf5add2475e4aa71c31e09090e06
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-01-03 14:07:18 -0700

    Fix test descriptions

--- test/election_test.exs
+++ test/election_test.exs
@@ -11,14 +11,14 @@ defmodule Ballot.ElectionTest do
     assert ["A"] = Ballot.Counting.plurality(election.votes)
   end
 
-  test "raises when vote types are mixed" do
+  test "returns error when vote types are mixed" do
     election = Ballot.Election.new(["A"])
     {:ok, election} = Ballot.Election.cast(election, Ballot.PluralityVote.new("A"))
 
     assert {:error, :wrong_vote_type} == Ballot.Election.cast(election, Ballot.ApprovalVote.new(["A"]))
   end
 
-  test "raises when a duplicate vote is added" do
+  test "returns error when a duplicate vote is added" do
     vote = Ballot.PluralityVote.new("A")
 
     election = Ballot.Election.new(["A"])
@@ -27,7 +27,7 @@ defmodule Ballot.ElectionTest do
     assert {:error, :duplicate_vote} == Ballot.Election.cast(election, vote)
   end
 
-  test "raises when a vote has candidates not in the election" do
+  test "returns error when a vote has candidates not in the election" do
     election = Ballot.Election.new(["A"])
 
     assert {:error, :candidate_not_in_election} == Ballot.Election.cast(election, Ballot.PluralityVote.new("B"))
