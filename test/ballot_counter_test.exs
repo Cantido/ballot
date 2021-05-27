7ee65b0eb26254855afb3f2e7fd5405f7caff705
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-05-27 17:32:37 -0600

    Add tests from a paper I'm reading

--- test/ballot_counter_test.exs
+++ test/ballot_counter_test.exs
@@ -1,4 +1,37 @@
 defmodule BallotCounterTest do
   use ExUnit.Case, async: true
   doctest BallotCounter
+
+  # Examples from https://plato.stanford.edu/entries/voting-methods/
+
+  @ranked_choice_ballot_distribution %{
+    ["A", "B", "C", "D"] => 7,
+    ["B", "C", "D", "A"] => 5,
+    ["D", "B", "C", "A"] => 4,
+    ["C", "D", "A", "B"] => 3,
+  }
+
+  test "plurality_with_runoff" do
+    ballots = generate_ballots(@ranked_choice_ballot_distribution)
+
+    assert "A" == BallotCounter.plurality_with_runoff(ballots)
+  end
+
+  test "instant_runoff" do
+    ballots = generate_ballots(@ranked_choice_ballot_distribution)
+
+    assert "D" == BallotCounter.instant_runoff(ballots)
+  end
+
+  test "coombs" do
+    ballots = generate_ballots(@ranked_choice_ballot_distribution)
+
+    assert "B" == BallotCounter.coombs(ballots)
+  end
+
+  defp generate_ballots(ballot_counts) do
+    Enum.flat_map(ballot_counts, fn {ballot, count} ->
+      Stream.repeatedly(fn -> ballot end) |> Enum.take(count)
+    end)
+  end
 end
